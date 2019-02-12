3162b815a9750485cd8e5e7f98689047f7f39a54
shhhh <luchuanqing@luchuanqingdeMacBook-Pro.local>
2019-02-12 00:17:12 +0000

    finish broadcast5..

---
+++ broadcast5/lib/lpl.ex
@@ -0,0 +1,38 @@
+defmodule Lpl do
+
+  def start(c, reliability) do
+    receive do
+      { :bind, index, pl_map } ->
+        send c, { :ur_pl, pl_map}
+        next(pl_map, index, c, nil, reliability)
+    end
+  end
+
+  def next(pl_map, index, c, beb, reliability) do
+
+    receive do
+
+      { :your_beb, b } ->
+        next(pl_map, index, c, b, reliability)
+
+      { :pl_send, { :broadcast, _, _ } = msg} ->
+        send c, msg
+        next(pl_map, index, c, beb, reliability)
+
+      { :pl_send, msg } ->
+        rand = DAC.random(100)
+        if rand <= reliability do
+          send c, msg
+        end
+        next(pl_map, index, c, beb, reliability)
+
+      { :pl_deliver, from } ->
+        rand = DAC.random(100)
+        if rand <= reliability do
+          send beb, {:beb_update, from}
+        end
+        next(pl_map, index, c, beb, reliability)
+    end
+
+  end
+end
